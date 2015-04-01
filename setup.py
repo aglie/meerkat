1c55c32b0d36ff4765087b986105ced0b963a24a
arkadiy <aglietto@gmail.com>
2015-04-01 15:55:41 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'meerkat',
   packages = ['meerkat'],
-  version = '0.2',
+  version = '0.2.1',
   description = 'A program for reciprocal space reconstruction',
   author = 'Arkadiy Simonov, Dmitry Logvinovich',
   author_email = 'arkadiy.simonov@alumni.ethz.ch',
@@ -10,4 +10,5 @@ setup(
 #  download_url = 
   keywords = ['crystallography', 'single crystal', 'reciprocal space reconstruction'],
   classifiers = [],
+  install_requires = ['fabio','h5py'],
 )
