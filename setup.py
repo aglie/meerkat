1191eeffe85dd5cdd423b5ef8a6c6e565889f038
arkadiy <aglietto@gmail.com>
2015-04-01 15:24:22 +0200

    added setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'meerkat',
+  packages = ['meerkat'],
+  version = '0.2',
+  description = 'A program for reciprocal space reconstruction',
+  author = 'Arkadiy Simonov, Dmitry Logvinovich',
+  author_email = 'arkadiy.simonov@alumni.ethz.ch',
+  url = 'https://github.com/aglie/meerkat.git',
+#  download_url = 
+  keywords = ['crystallography', 'single crystal', 'reciprocal space reconstruction'],
+  classifiers = [],
+)
